05ce878ad03337ea23f10c4aa1367ccef6ad2506
Vishal Singh Bardewa <bardewavishal@gmail.com>
2025-07-14 11:17:05 +0530

    Updated floating nav design

--- app/components/organisms/ScrollingLogos.tsx
+++ app/components/organisms/ScrollingLogos.tsx
@@ -9,7 +9,7 @@ export default function ScrollingLogos() {
                         ul.insertAdjacentHTML('afterend', ul.outerHTML);
                         ul.nextSibling.setAttribute('aria-hidden', 'true');
                     })"
-      className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
+      className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)] my-6"
     >
       <ul
         x-ref="logos"
