60a7ec2a034af5996364bf4bfb0b5cee5220f801
Vishal Singh Bardewa <bardewavishal@gmail.com>
2025-07-21 15:52:06 +0530

    Updated config

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'plus.unsplash.com'
+      },
+      {
+        protocol: 'https',
         hostname: 'studio.tailwindui.com',
       },
     ],
