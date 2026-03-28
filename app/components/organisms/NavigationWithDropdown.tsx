e5f2d5104262513e9ae11532219b17ca71433f30
Vishal Singh Bardewa <bardewavishal@gmail.com>
2026-03-28 05:34:46 +0530

    fix: mobile navigation fix

--- app/components/organisms/NavigationWithDropdown.tsx
+++ app/components/organisms/NavigationWithDropdown.tsx
@@ -16,7 +16,7 @@ export default function NavigationWithDropdown({ navigation }: any) {
     <div className="bg-white pb-4">
       {/* Mobile menu */}
       <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
+        <Dialog as="div" className="relative z-50 lg:hidden" onClose={setOpen}>
           <Transition.Child
             as={Fragment}
             enter="transition-opacity ease-linear duration-300"
