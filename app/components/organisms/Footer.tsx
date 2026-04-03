cdc464fa9f3374374ae70bacc957b7a8fde9825c
Vishal Singh Bardewa <bardewavishal@gmail.com>
2026-04-03 12:35:08 +0530

    fix: json-ld

--- app/components/organisms/Footer.tsx
+++ app/components/organisms/Footer.tsx
@@ -102,7 +102,7 @@ export default function Footer() {
                   ))}
                 </ul>
               </div>
-              <div className="mt-10 md:mt-0" itemScope itemType="http://schema.org/LocalBusiness">
+              <div className="mt-10 md:mt-0">
                 <h3 className="text-sm font-semibold leading-6 text-gray-900">
                   Contact Us
                 </h3>
@@ -113,9 +113,9 @@ export default function Footer() {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                       <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                     </svg>
-                    <address className="not-italic" itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
-                      <p itemProp="streetAddress">First Floor, Vikiye Center,<br />Opp. Notun Bosti Gate, Fellowship Colony</p>
-                      <p><span itemProp="addressLocality">Dimapur</span>, <span itemProp="addressRegion">Nagaland</span> <span itemProp="postalCode">797112</span></p>
+                    <address className="not-italic">
+                      <p>First Floor, Vikiye Center,<br />Opp. Notun Bosti Gate, Fellowship Colony</p>
+                      <p><span>Dimapur</span>, <span>Nagaland</span> <span>797112</span></p>
                     </address>
                   </div>
                   <div className="flex gap-x-3">
@@ -123,7 +123,7 @@ export default function Footer() {
                     <svg className="h-5 w-5 flex-none text-brandpurple" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                     </svg>
-                    <a href="tel:+918798587779" className="hover:text-gray-900" itemProp="telephone">+91-8798-587779</a>
+                    <a href="tel:+918798587779" className="hover:text-gray-900">+91-8798-587779</a>
                   </div>
                 </div>
               </div>
