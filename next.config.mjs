884176c2ea0f766bbe63198d444375364b128be8
Vishal Bardewa <bardewavishal@gmail.com>
2026-08-17 02:45:20 +0530

    fix: SEO audit fix

--- next.config.mjs
+++ next.config.mjs
@@ -121,7 +121,7 @@ const nextConfig = {
           },
           {
             key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+            value: "camera=(), microphone=(), geolocation=()",
           },
           {
             key: "Strict-Transport-Security",
